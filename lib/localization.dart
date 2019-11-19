2dbad12f07bfa52990ad363c3b70b3b9852dfbbb
rafaeldcampbell <rafaelcampbell@id.uff.br>
2019-11-19 08:38:56 -0300

    Adicionando testes para internacionalização (e para texto de widget)

--- lib/localization.dart
+++ lib/localization.dart
@@ -5,19 +5,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class Localization {
+
   Localization(this._locale);
 
   final Locale _locale;
+  Map<String, dynamic> _sentences;
 
   static Localization of(BuildContext context) {
     return Localizations.of<Localization>(context, Localization);
   }
 
-  Map<String, dynamic> _sentences;
-
   Future<bool> load() async {
     String data = await rootBundle.loadString('assets/res/string/string_${_locale.languageCode}.json');
-
     this._sentences = json.decode(data);
     return true;
   }
@@ -31,14 +30,26 @@ class Localization {
 }
 
 class LocalizationDelegate extends LocalizationsDelegate<Localization> {
-  const LocalizationDelegate();
+  
+  const LocalizationDelegate({
+    this.isTest = false,
+    this.testLocale = const Locale('pt'),
+
+  });
+  final bool isTest;
+  final Locale testLocale;
 
   @override
   bool isSupported(Locale locale) => ['en', 'pt'].contains(locale.languageCode);
 
   @override
   Future<Localization> load(Locale locale) async {
-    Localization localizations = new Localization(locale);
+    Localization localizations;
+    if (this.isTest) {
+      localizations = new Localization(this.testLocale);
+    } else {
+      localizations = new Localization(locale);
+    }
     await localizations.load();
     return localizations;
   }
