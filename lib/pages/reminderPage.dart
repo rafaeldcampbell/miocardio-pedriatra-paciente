df46a32a38e44eb8cc15814f4fb84ec17b325909
Aline Lima <lima_aline@id.uff.br>
2019-11-22 18:53:02 -0300

    Criação do Calendário
    Calendário na página de Lembretes foi criado

--- lib/pages/reminderPage.dart
+++ lib/pages/reminderPage.dart
@@ -1,32 +1,58 @@
 import 'package:flutter/material.dart';
-import 'package:miocardio_paciente/functions/localization.dart' show Localization;
+import 'package:miocardio_paciente/functions/localization.dart'
+    show Localization;
+import 'package:table_calendar/table_calendar.dart';
 
 class ReminderPage extends StatefulWidget {
   ReminderPageState createState() => ReminderPageState();
 }
 
 class ReminderPageState extends State<ReminderPage> {
+  CalendarController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = CalendarController();
+  }
+
   @override
   Widget build(BuildContext context) {
     var localization = Localization.of(context);
     return Scaffold(
-      backgroundColor: Color.fromRGBO(253, 224, 224, 1),
-      body: ListView.builder(
-          itemCount: 1,
-          padding: EdgeInsets.all(5),
-          itemBuilder: (BuildContext context, int index) => Container(
-                padding: EdgeInsets.only(top: 30, bottom: 10),
-                child: Text(
-                  localization.trans('pagetitleReminder'),
-                  key: Key("pagetitleReminder"),
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              )),
-    );
+        backgroundColor: Color.fromRGBO(253, 224, 224, 1),
+        body: bodyData(localization));
   }
+
+  Widget bodyData(localization) => Scaffold(
+        backgroundColor: Color.fromRGBO(253, 224, 224, 1),
+        appBar: AppBar(
+          backgroundColor: Color.fromRGBO(253, 224, 224, 1),
+          title: title(localization),
+        ),
+        body: Container(
+          child: Column(
+            children: <Widget>[calendar()],
+          ),
+        ),
+      );
+
+  Widget title(localization) => Center(
+        child: Text(localization.trans('pagetitleReminder'),
+            key: Key("pagetitleReminder"),
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+            )),
+      );
+
+ //Retorna o calendario da pagina
+  Widget calendar() => TableCalendar(
+        calendarStyle: CalendarStyle(
+          todayColor: Colors.purpleAccent,
+        ),
+        calendarController: _controller,
+      );
 }
