eb39a4d6ef1ce4f92ff2c37014854ec7586b3667
rafaeldcampbell <rafaelcampbell@id.uff.br>
2019-11-22 00:30:51 -0300

    Construindo estrutura base da página de informação

--- lib/pages/info.dart
+++ lib/pages/info.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:miocardio_paciente/functions/localization.dart' show Localization;
 
 class Info extends StatefulWidget {
-  InfoState createState() => InfoState();
+  @override
+  State<StatefulWidget> createState() {
+    return InfoState();
+  }
 }
 
 class InfoState extends State<Info> {
@@ -11,22 +14,77 @@ class InfoState extends State<Info> {
     var localization = Localization.of(context);
     return Scaffold(
       backgroundColor: Color.fromRGBO(253, 224, 224, 1),
-      body: ListView.builder(
-          itemCount: 1,
-          padding: EdgeInsets.all(5),
-          itemBuilder: (BuildContext context, int index) => Container(
-                padding: EdgeInsets.only(top: 30, bottom: 10),
-                child: Text(
-                  localization.trans('pagetitleInfo'),
-                  key : Key("pagetitleInfo"),
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              )),
+      body: 
+        bodyData(localization)
     );
   }
+
+  Widget bodyData(localization) =>
+        DefaultTabController(
+          length: 2,
+          child: Scaffold(
+            backgroundColor: Color.fromRGBO(253, 224, 224, 1),
+            appBar: AppBar(
+              backgroundColor: Color.fromRGBO(253, 224, 224, 1),
+              bottom: WhiteTabBar(),
+              title: title(localization),
+            ),
+            body: TabBarView(
+              children: [
+                pageSymptoms(localization),
+                pageActivities(localization),
+              ],
+            ),
+          ),
+        );
+
+  Widget title(localization) =>
+  Center(
+    child: 
+      Text(
+          localization.trans('pagetitleInfo'),
+          key : Key("pagetitleInfo"),
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+          )
+      ),
+    );
+
+  Widget pageSymptoms(localization) =>
+    Scaffold(
+      backgroundColor: Color.fromRGBO(253, 224, 224, 1),
+    );
+
+  Widget pageActivities(localization) =>
+    Scaffold(
+      backgroundColor: Color.fromRGBO(253, 224, 224, 1),
+    );
+    
+}
+
+class WhiteTabBar extends Container implements PreferredSizeWidget {
+  WhiteTabBar();
+  final Color _color = Color.fromRGBO(255, 255, 255, 1);
+  final TabBar _tabBar = TabBar(
+          labelStyle: TextStyle(fontSize: 19.0, fontWeight: FontWeight.bold),
+          indicatorColor: Color.fromRGBO(255, 100, 100, 1),
+          labelColor: Color.fromRGBO(0, 0, 0, 1),
+          unselectedLabelColor: Color.fromRGBO(0, 0, 0, 0.5),
+          tabs: [
+            Tab(text: "Sintomas"),
+            Tab(text: "Atividades"),
+          ],
+        );
+
+  @override
+  Size get preferredSize => _tabBar.preferredSize;
+
+  @override
+  Widget build(BuildContext context) => Container(
+    color: _color,
+    child: _tabBar,
+  );
 }
